c6df535ec33677a9768a5d7b9ad9c6d265196889
Diego Nascimento Santana <Diego Nascimento Santana>
2022-02-07 20:38:42 -0300

    Interface praticamente completa 🙂

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
+import { Container } from "./modal.styles";
 
 interface newTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +12,26 @@ export function NewTransactionModal({
   onRequestClose,
 }: newTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button type="button" onClick={onRequestClose} className="modal-close">
+        {" "}
+        <img src={closeImg} alt="Fechar modal" />{" "}
+      </button>
+      <Container>
+        <h2>Cadastrar despesa </h2>
+
+        <input placeholder="Titulo" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
